1932608c72745612162b8f0ce0e6fa9526ce7ef2
ownue <enu_i@naver.com>
2026-01-31 11:10:42 +0900

    feat: 초기 설정 저장 로직 구현

--- src/main/java/com/example/peakly/domain/user/service/UserService.java
+++ src/main/java/com/example/peakly/domain/user/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.peakly.domain.user.service;
 
+import com.example.peakly.domain.user.dto.request.InitialSettingRequest;
+import com.example.peakly.domain.user.dto.response.InitialSettingResponse;
+
 public interface UserService {
+    InitialSettingResponse saveInitialSetting(Long userId, InitialSettingRequest req);
 }
