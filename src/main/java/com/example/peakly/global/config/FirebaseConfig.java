7f1714e43c3c5b612ce15cfeb91b3927c34f273f
SungMinju <minju2928@naver.com>
2026-02-04 17:43:02 +0900

    [Feat] : FCM 파이어베이스 설정

---
+++ src/main/java/com/example/peakly/global/config/FirebaseConfig.java
@@ -0,0 +1,30 @@
+package com.example.peakly.global.config;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.credentials.path}")
+    private Resource serviceAccount;
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (!FirebaseApp.getApps().isEmpty()) return; // 중복 초기화 방지
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
