95cd46eb24546fc1d5e55154231d2fe8a87fc787
ownue <enu_i@naver.com>
2026-02-13 21:47:59 +0900

    chore: 불필요한 어노테이션 제거

--- src/main/java/com/example/peakly/domain/user/service/UserService.java
+++ src/main/java/com/example/peakly/domain/user/service/UserService.java
@@ -1,8 +1,4 @@
 package com.example.peakly.domain.user.service;
 
-import com.example.peakly.domain.user.dto.request.InitialDataCreateRequest;
-import com.example.peakly.domain.user.dto.response.InitialDataCreateResponse;
-import jakarta.validation.constraints.NotNull;
-
 public interface UserService {
 }
--- src/main/java/com/example/peakly/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/peakly/domain/user/service/UserServiceImpl.java
@@ -1,27 +1,8 @@
 package com.example.peakly.domain.user.service;
 
-import com.example.peakly.domain.user.command.InitialDataCreateCommand;
-import com.example.peakly.domain.user.dto.request.InitialDataCreateRequest;
-import com.example.peakly.domain.user.dto.response.InitialDataCreateResponse;
-import com.example.peakly.domain.user.entity.InitialData;
-import com.example.peakly.domain.user.entity.User;
-import com.example.peakly.domain.user.repository.InitialDataRepository;
-import com.example.peakly.domain.user.repository.UserRepository;
-import com.example.peakly.global.apiPayload.code.status.UserErrorStatus;
-import com.example.peakly.global.apiPayload.exception.GeneralException;
-import jakarta.validation.Valid;
-import org.springframework.transaction.annotation.Transactional;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
-
-import java.time.ZoneId;
 
 @Service
-@Validated
-@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
 }
