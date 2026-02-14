728a6ba145cf88a66576cccb83ae015bed78d72e
ownue <enu_i@naver.com>
2026-02-14 16:33:04 +0900

    refactor: 이메일 인증 링크 클릭 시 인증 가능하도록 수정

--- src/main/java/com/example/peakly/domain/auth/service/AuthService.java
+++ src/main/java/com/example/peakly/domain/auth/service/AuthService.java
@@ -4,7 +4,6 @@ import com.example.peakly.domain.auth.dto.request.CheckEmailRequest;
 import com.example.peakly.domain.auth.dto.request.LoginRequest;
 import com.example.peakly.domain.auth.dto.request.SignupRequest;
 import com.example.peakly.domain.auth.dto.request.EmailVerifySendRequest;
-import com.example.peakly.domain.auth.dto.request.EmailVerifyRequest;
 import com.example.peakly.domain.auth.dto.response.CheckEmailResponse;
 import com.example.peakly.domain.auth.dto.response.SignupResponse;
 import com.example.peakly.domain.auth.dto.response.LoginResponse;
@@ -13,9 +12,12 @@ import com.example.peakly.domain.auth.dto.response.EmailVerifyResponse;
 
 public interface AuthService {
     CheckEmailResponse checkEmail(CheckEmailRequest req);
+
     SignupResponse signup(SignupRequest req);
+
     LoginResponse login(LoginRequest req);
 
     EmailVerifySendResponse sendEmailVerify(EmailVerifySendRequest req);
-    EmailVerifyResponse verifyEmail(EmailVerifyRequest req);
+
+    EmailVerifyResponse verifyEmail(String token);
 }
