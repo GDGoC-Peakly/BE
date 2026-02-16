655059bbf5c5a89e86c39355f76787132ffbfe05
ownue <enu_i@naver.com>
2026-02-16 15:12:00 +0900

    chore: 오탈자 수정

--- src/main/java/com/example/peakly/domain/focusSession/entity/FocusSession.java
+++ src/main/java/com/example/peakly/domain/focusSession/entity/FocusSession.java
@@ -152,7 +152,7 @@ public class FocusSession extends BaseEntity {
 
     public void markRunning() {
         if (this.sessionStatus != SessionStatus.PAUSED) {
-            throw new IllegalStateException("PAUSED 상태에서만 RUNNING으롤 바꿀 수 있습니다.");
+            throw new IllegalStateException("PAUSED 상태에서만 RUNNING으로 바꿀 수 있습니다.");
         }
         this.sessionStatus = SessionStatus.RUNNING;
     }
