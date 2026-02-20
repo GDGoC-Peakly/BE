613be44c812d9539c69032a4c42cb6b7f189557d
ownue <enu_i@naver.com>
2026-02-21 02:59:27 +0900

    chore: 안 쓰는 어노테이션 제거

--- src/main/java/com/example/peakly/domain/home/service/HomeSummaryServiceImpl.java
+++ src/main/java/com/example/peakly/domain/home/service/HomeSummaryServiceImpl.java
@@ -12,7 +12,6 @@ import com.example.peakly.domain.peakTimePrediction.repository.PeakTimePredictio
 import com.example.peakly.global.apiPayload.code.status.HomeErrorStatus;
 import com.example.peakly.global.apiPayload.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HomeSummaryServiceImpl implements HomeSummaryService {
